7e537ddaffc9c3dd5326b278ec3cadbe3130ba7e
Arseni <esenin.magmel@gmail.com>
2025-12-13 19:37:11 +0300

    create taskManager app

--- ComposeBasicsPractice/app/src/main/java/by/arsy/composebasicspractice/MainActivity.kt
+++ ComposeBasicsPractice/app/src/main/java/by/arsy/composebasicspractice/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import by.arsy.composebasicspractice.ui.theme.ComposeBasicsPracticeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             ComposeBasicsPracticeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    CompleteAllTasksScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +40,52 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun CompleteAllTasksScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier.fillMaxSize()
+    ) {
+        CompleteAllTasksLabel(
+            painter = painterResource(R.drawable.ic_task_completed),
+            allTasksCompletedText = stringResource(R.string.all_tasks_completed),
+            niceWorkText = stringResource(R.string.nice_work)
+        )
+    }
+}
+
+@Composable
+fun CompleteAllTasksLabel(
+    painter: Painter,
+    allTasksCompletedText: String,
+    niceWorkText: String,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
-    )
+    ) {
+        Image(
+            painter = painter,
+            contentDescription = null
+        )
+        Text(
+            text = allTasksCompletedText,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier
+                .padding(top = 24.dp)
+                .padding(bottom = 8.dp)
+        )
+        Text(
+            text = niceWorkText,
+            fontSize = 16.sp,
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun CompleteAllTasksScreenPreview() {
     ComposeBasicsPracticeTheme {
-        Greeting("Android")
+        CompleteAllTasksScreen()
     }
 }
