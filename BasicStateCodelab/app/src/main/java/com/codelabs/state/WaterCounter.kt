b25b3c783b3e79f654a4d7b9c68c055523e8833d
Arseni <esenin.magmel@gmail.com>
2025-12-17 18:37:17 +0300

    use remember

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -21,23 +21,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count = mutableStateOf(value = 0)
+        var count by remember { mutableStateOf(value = 0) }
         Text(
-            text = "You've had ${count.value} glasses.",
+            text = "You've had $count glasses.",
             modifier = Modifier.padding(bottom = 16.dp)
         )
-        Log.d("WaterCounter", "Count from compose: ${count.value}")
+        Log.d("WaterCounter", "Count from compose: $count")
         Button(
             onClick = {
-                count.value++
-                Log.d("WaterCounter", "Count from button: ${count.value}")
+                count++
+                Log.d("WaterCounter", "Count from button: $count")
             }
         ) {
             Text(text = "Add one")
