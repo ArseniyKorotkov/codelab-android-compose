8c7b4ba66c1991028e73f945c87e6538a940e2e7
Arseni <esenin.magmel@gmail.com>
2025-12-17 18:32:21 +0300

    mutableState without remember

--- BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
+++ BasicStateCodelab/app/src/main/java/com/codelabs/state/WaterCounter.kt
@@ -21,22 +21,23 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        val count = mutableStateOf(value = 0)
         Text(
-            text = "You've had $count glasses.",
+            text = "You've had ${count.value} glasses.",
             modifier = Modifier.padding(bottom = 16.dp)
         )
-        Log.d("WaterCounter", "Count from compose: $count")
+        Log.d("WaterCounter", "Count from compose: ${count.value}")
         Button(
             onClick = {
-                count++
-                Log.d("WaterCounter", "Count from button: $count")
+                count.value++
+                Log.d("WaterCounter", "Count from button: ${count.value}")
             }
         ) {
             Text(text = "Add one")
